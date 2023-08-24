900f0e49f04478970d38e0d9767ffa1edd5609ee
Jason Mitchell <jason.mitchell@autogeneral.com.au>
2023-08-24 12:06:08 +1000

    adding vue components to site

---
+++ src/components/Food.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="item in food">
+                {{ item }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script setup lang="ts">
+const food = ["Pizza", "French fries", "Spaghetti"];
+</script>
